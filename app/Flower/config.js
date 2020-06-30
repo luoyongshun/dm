65a5dab7056e766f664ef25e40d472b46b7fdd5b
罗永顺 <luoyongshun@wanpinghui.com>
2020-06-30 15:00:22 +0800

    Flower commit

---
+++ app/Flower/config.js
@@ -0,0 +1,6 @@
+
+const config = {
+    api_base_url: 'http://bl.7yue.pro/v1/',
+    appkey: "ewi98tEqZP9AN7cY",
+}
+export {config}
---
+++ app/Flower/models/like.js
@@ -0,0 +1,27 @@
+import HTTP from '../util/http.js'
+
+class LikeModel extends HTTP {
+    like(behavior, artID, category){
+        let url = behavior == 'like' ? 'like' : 'like/cancel'
+        this.request({
+            method:'POST',
+            url:url,
+            data: {
+                art_id: artID,
+                type: category
+            }
+        })
+    }
+
+    getClassicLikeStatus(artID,category,sCallback){
+        this.request({
+            url:'classic/' + category + '/' + artID + '/favor',
+            success:sCallback
+        })
+    }
+
+}
+
+export {
+    LikeModel
+}
