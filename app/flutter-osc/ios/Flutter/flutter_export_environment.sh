75b79c579c473ba94b1311c48d3b005690611012
罗永顺 <luoyongshun@wanpinghui.com>
2020-06-30 16:04:47 +0800

    flutter-osc

---
+++ app/flutter-osc/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/luoyongshun/Downloads/下载的文件/开发语言/flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/luoyongshun/dm/app/flutter-osc"
+export "FLUTTER_TARGET=/Users/luoyongshun/dm/app/flutter-osc/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/luoyongshun/Downloads/下载的文件/开发语言/flutter/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
