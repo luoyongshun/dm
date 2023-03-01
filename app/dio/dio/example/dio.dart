c230cc818a3ab7ec009c68783b0282aa33ed6f16
richard <648508153@qq.com>
2023-03-01 11:18:56 +0800

    Delete app/dio directory

--- app/dio/dio/example/dio.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dio/dio.dart';
-
-/// More examples see https://github.com/flutterchina/dio/tree/master/example
-main() async {
-  var dio = Dio();
-  Response response = await dio.get('https://google.com');
-  print(response.data);
-}
